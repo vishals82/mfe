8c17808751e1c6a5cd579dfd0db960eb62043976
vishals82 <vishalsingla82@gmail.com>
2022-02-04 16:13:37 -0500

    doc4

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/auth/latest/',
+    publicPath: '/mfe/auth/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/dashboard/latest/',
+    publicPath: '/mfe/dashboard/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
